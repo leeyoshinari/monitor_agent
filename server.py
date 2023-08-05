dc9b300b385fa6d1b4470bcc16f91f6140bcb661
leeyoshinari <lee_jc@outlook.com>
2023-08-05 10:02:25 +0800

    fix

--- server.py
+++ server.py
@@ -4,9 +4,9 @@
 
 import os
 import time
-import asyncio
+import json
 import traceback
-from aiohttp import web
+from flask import Flask, Response
 from logger import logger, cfg
 from performance_monitor import PerMon, port_to_pid
 
@@ -32,6 +32,7 @@ def get_ip():
     return ip
 
 
+app = Flask(__name__)
 permon = PerMon()
 HOST = get_ip()
 PID = os.getpid()
@@ -39,48 +40,47 @@ with open('pid', 'w', encoding='utf-8') as f:
     f.write(str(PID))
 
 
-async def index(request):
+@app.route('/', methods=['GET'])
+def index():
     """
     Home, basic data can be displayed by visiting http://ip:port
     :param request:
     :return:
     """
-    return web.Response(
-        body=f'The server system version is {permon.system_version}, {permon.cpu_info}, total memory is {permon.total_mem}G, '
+    return Response(status=200,
+        response=f'The server system version is {permon.system_version}, {permon.cpu_info}, total memory is {permon.total_mem}G, '
              f'the network card is {permon.nic}, bandwidth is {permon.network_speed}Mb/s, {len(permon.all_disk)} disks, '
              f'total size of disks is {permon.total_disk_h}, disks number is {"、".join(permon.all_disk)}. '
              f'If you need to stop the monitoring agent, please visit http://{HOST}:{cfg.getServer("port")}/stop')
 
 
-async def run_monitor(request):
-    """
-    Start monitoring port
-    :param request:
-    :return:
-    """
-    try:
-        data = await request.json()
-        is_run = data.get('isRun')
-        permon.start = int(is_run)
-
-    except Exception as err:
-        logger.error(traceback.format_exc())
-        return web.json_response({'code': 2, 'msg': str(err), 'data': {'host': HOST, 'port': None, 'pid': None}})
+# @app.route('/runMonitor/<isRun>', methods=['GET'])
+# def run_monitor(isRun):
+#     """
+#     Start monitoring port
+#     :param request:
+#     :return:
+#     """
+#     try:
+#         permon.start = int(isRun)
+#     except Exception as err:
+#         logger.error(traceback.format_exc())
+#         return web.json_response({'code': 2, 'msg': str(err), 'data': {'host': HOST, 'port': None, 'pid': None}})
 
 
-async def get_gc(request):
+@app.route('/getGC/<port>', methods=['GET'])
+async def get_gc(port):
     """
     Get GC data of java application
-    :param request:
+    :param port:
     :return:
     """
-    port = request.match_info['port']
     try:
         pid = port_to_pid(port)
         if pid is None:
             logger.warning(f"Port {port} not started!")
-            return web.json_response({'code': 1, 'msg': f"Port {port} not started!", 'data': None})
-
+            return Response(response=json.dumps({'code': 1, 'msg': f"Port {port} not started!", 'data': None}),
+                            status=200, mimetype='application/json')
         result = os.popen(f'jstat -gc {pid} |tr -s " "').readlines()[1]
         res = result.strip().split(' ')
 
@@ -107,27 +107,14 @@ async def get_gc(request):
                 ffgc = round(time.time() - fgc_time_history[-1], 2)
         else:
             fgc = -1
-
-    except Exception:
+        del res, fgc_time_history
+    except:
         logger.error(traceback.format_exc())
         ygc, ygct, fgc, fgct, fygc, ffgc = -1, -1, -1, -1, '-', -1
 
-    return web.json_response({'code': 0, 'msg': 'Successful!', 'data': [ygc, ygct, fgc, fgct, fygc, ffgc]})
+    return Response(response=json.dumps({'code': 0, 'msg': 'Successful!', 'data': [ygc, ygct, fgc, fgct, fygc, ffgc]}),
+                    status=200, mimetype='application/json')
 
 
-async def main():
-    app = web.Application()
-
-    app.router.add_route('GET', '/', index)
-    app.router.add_route('POST', '/runMonitor', run_monitor)
-    app.router.add_route('GET', '/getGC/{port}', get_gc)
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, HOST, cfg.getServer('port'))
-    await site.start()
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-loop.run_forever()
+if __name__ == '__main__':
+    app.run(host=HOST, port=cfg.getServer('port'), debug=False)
